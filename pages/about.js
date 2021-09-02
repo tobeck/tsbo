fed37839b884df8016145b3848efb2159c3e5124
Tobias Becker-Olsson <tbecker@MacBook-Air.tsbo.me>
2021-09-02 20:58:40 +0200

    Added about me page

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import profilePic from '../public/images/profile.jpg'
+
+function About() {
+    return (
+      <div>
+          <h2>About Me.</h2>
+          <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum, ante quis imperdiet gravida, velit enim interdum urna, a placerat nisl turpis eget felis. Etiam sed nunc tincidunt, sollicitudin neque ac, hendrerit ex. Etiam sit amet faucibus elit. Maecenas viverra fermentum odio a semper. Vestibulum interdum orci ut sem mattis fermentum. Quisque eget mi semper purus fermentum tincidunt nec in tortor. Cras nec aliquet lorem.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur molestie, nisi auctor viverra bibendum, enim nisl sagittis eros, eget interdum risus tortor non ante. Integer auctor nibh vitae mi commodo sollicitudin. Etiam eu diam quis neque ultrices feugiat quis eget justo. Quisque convallis libero sit amet euismod elementum. Maecenas fermentum nec elit nec semper. Sed mattis eget libero sed ultrices. Suspendisse potenti. Quisque eleifend malesuada ligula ac pretium. Nam vel elit diam. Curabitur laoreet pulvinar nisi, sit amet tincidunt dolor mattis sed. Nunc pretium dictum nibh ac ullamcorper. Donec vehicula odio id lectus lobortis, quis consequat urna fringilla. Aliquam mollis massa ut auctor porttitor. Aliquam erat volutpat.</p>
+          </div>
+      </div>
+    )
+  }
+  
+  export default About
