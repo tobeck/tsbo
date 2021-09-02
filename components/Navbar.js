1c780c1219c78811142c3ad3f8e1b81155ce607f
Tobias Becker-Olsson <tbecker@MacBook-Air.tsbo.me>
2021-09-02 20:58:09 +0200

    Tailwind layout change for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,13 +4,15 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav>
-            <Header />
-            <div  className='container'>
+            <div  className='flex flex-row justify-end gap-2 text-sm font-bold text-bistre'>
                 <Link href='/'>
                     <a>Home</a>
                 </Link>
                 <Link href='/blog'>
                     <a>Blog</a>
+                </Link>
+                <Link href='/about'>
+                    <a>About</a>
                 </Link>
             </div>
         </nav>
