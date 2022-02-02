d0cea435b72767d22fcfc6228fce0f5337421976
tobeck <tobias.becker.olsson@gmail.com>
2022-02-02 10:58:15 +0100

    Added About and Experience pages

---
+++ components/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <>
+            <h1>Tobias Becker</h1>
+            <h2>Building Scalable and Maintainable Platforms</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fermentum, ante quis imperdiet gravida, velit enim interdum urna, a placerat nisl turpis eget felis. Etiam sed nunc tincidunt, sollicitudin neque ac, hendrerit ex. Etiam sit amet faucibus elit. Maecenas viverra fermentum odio a semper. Vestibulum interdum orci ut sem mattis fermentum. Quisque eget mi semper purus fermentum tincidunt nec in tortor. Cras nec aliquet lorem.</p>
+        </>
+    )
+}
