c814c7d715cc71a4139bc3e5d4b341da7f240b92
tobeck <tobias.becker.olsson@gmail.com>
2022-02-02 10:58:39 +0100

    Import fontawesome fab icons, test bounce on hover

--- components/SocialsBar.js
+++ components/SocialsBar.js
@@ -2,19 +2,29 @@ import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function SocialsBar() {
-    return (
-        <nav>
-            <div className='flex flex-col place-item-end gap-4'>
-                <Link href='https://www.linkedin.com/in/tobias-becker-olsson-9a678252'>
-                    <a target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin-in']} size='lg' /></a>
-                </Link>
-                <Link href='https://www.github.com/tobeck'>
-                    <a target='_blank'><FontAwesomeIcon icon={['fab', 'github']} size='lg' /></a>
-                </Link>
-                <Link href='https://www.instagram.com'>
-                    <a target='_blank'><FontAwesomeIcon icon={['fab', 'instagram']} size='lg' /></a>
-                </Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="flex flex-col p-6 absolute bottom-0 left-0 gap-4">
+        <Link href="https://www.linkedin.com/in/tobias-becker-olsson-9a678252">
+          <a target="_blank">
+            <FontAwesomeIcon
+              className="animate-bounce"
+              icon={['fab', 'linkedin']}
+              size="lg"
+            />
+          </a>
+        </Link>
+        <Link href="https://www.github.com/tobeck">
+          <a target="_blank">
+            <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
+          </a>
+        </Link>
+        <Link href="https://www.instagram.com/tbecker">
+          <a target="_blank">
+            <FontAwesomeIcon icon={['fab', 'instagram']} size="lg" />
+          </a>
+        </Link>
+      </div>
+    </nav>
+  )
 }
