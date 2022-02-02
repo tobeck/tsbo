3071d4acc0c2a9ed58a29b39136610afea815428
tobeck <tobias.becker.olsson@gmail.com>
2022-02-02 10:56:15 +0100

    Use Layout component in index instead of _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,9 @@
-import Layout from '../components/Layout'
 import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fab)
 
-function MyApp({Component, pageProps}) {
-    return (
-        <Layout>
-            <>
-                <main>
-                    <Component {...pageProps} />
-                </main>
-            </>
-        </Layout>
-    )
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp
