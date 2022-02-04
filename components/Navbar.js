72755233a52c11058cad8a5d768389bc68d97419
tobeck <tobias.becker.olsson@gmail.com>
2022-02-04 19:50:16 +0100

    Navbar, added experience, rm About redundant

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,17 @@
-import Header from "./Header"
 import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <nav>
-            <div  className='flex flex-row justify-end gap-2 text-sm font-bold text-bistre'>
+            <div className='flex flex-row justify-end gap-2 text-sm font-bold text-bistre'>
                 <Link href='/'>
                     <a>Home</a>
                 </Link>
+                <Link href='/experiences'>
+                    <a>Experiences</a>
+                </Link>
                 <Link href='/blog'>
                     <a>Blog</a>
-                </Link>
-                <Link href='/about'>
-                    <a>About</a>
                 </Link>
             </div>
         </nav>
