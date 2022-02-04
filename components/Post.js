9f1be38d9394d597c2af3e1f748459a4804fb08c
tobeck <tobias.becker.olsson@gmail.com>
2022-02-04 19:49:19 +0100

    Update and fix issue with new path for blog posts, twcss form

--- components/Post.js
+++ components/Post.js
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function Post({post}) {
     return (
-        <div className='card'>
-            <img src={post.frontmatter.cover_image} alt='' />
-            <div className='post-date'>Posted on {post.frontmatter.date}</div>
-            <h3>{post.frontmatter.title}</h3>
-            <p>{post.frontmatter.excerpt}</p>
+        <div>
+            <img className='rounded' src={post.frontmatter.cover_image} alt='' />
+            <div className='text-sm text-gray-500'>Posted on {post.frontmatter.date}</div>
+            <h3 className='text-xl'>{post.frontmatter.title}</h3>
+            <p className='text-sm'>{post.frontmatter.excerpt}</p>
             <Link href={`/blog/${post.slug}`}>
-                <a className='btn'>Read More</a>
+                <a>Read More</a>
             </Link>
         </div>
     )
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,54 +4,62 @@ import matter from 'gray-matter'
 import marked from 'marked'
 import Link from 'next/link'
 
-export default function PostPage({frontmatter: {title, date, cover_image}, slug, content}) {
-    return <>
-        <Link href='/blog'>
-            <a>Go Back</a>
-        </Link>
+export default function PostPage({
+  frontmatter: { title, date, cover_image },
+  slug,
+  content,
+}) {
+  return (
+    <>
+      <Link href="/blog">
+        <a>Go Back</a>
+      </Link>
+      <div>
+        <h1 className="text-4xl">{title}</h1>
+        <div className="text-sm text-gray-500">Posted on {date}</div>
+        <img src={cover_image} alt=""></img>
         <div>
-            <h1 className='text-4xl'>{title}</h1>
-            <div className='text-sm text-gray-500'>Posted on {date}</div>
-            <img src={cover_image} alt=''></img>
-            <div>
-                <div dangerouslySetInnerHTML={{ __html: marked (content)}}></div>
-            </div>
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
+      </div>
     </>
+  )
 }
 
 // Generate a list of paths to files in the posts directory.
 export async function getStaticPaths() {
-    const files = fs.readdirSync(path.join('posts'))
+  const files = fs.readdirSync(path.join('_posts'))
 
-    const paths = files.map(filename => ({
-        params: {
-            slug: filename.replace('.md', '')
-        }
-    }))
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }))
 
-    // Return an object with paths.
-    return {
-        paths,
-        // Fallback returns 404 not found.
-        fallback: false
-    }
+  // Return an object with paths.
+  return {
+    paths,
+    // Fallback returns 404 not found.
+    fallback: false,
+  }
 }
 
 // Retrieve the content of the files found in getStaticPaths.
-export async function getStaticProps({params: {slug}}) {
-    // Fetch file data based on slug.
-    const markdownWithMeta = fs.readFileSync(path.join(
-        'posts', slug + '.md'), 'utf-8')
-    
-    // Get data from frontmatter
-    const {data:frontmatter, content} = matter(markdownWithMeta)
+export async function getStaticProps({ params: { slug } }) {
+  // Fetch file data based on slug.
+  const markdownWithMeta = fs.readFileSync(
+    path.join('_posts', slug + '.md'),
+    'utf-8'
+  )
 
-    return {
-        props: {
-            frontmatter,
-            slug,
-            content,
-        },
-    }
+  // Get data from frontmatter
+  const { data: frontmatter, content } = matter(markdownWithMeta)
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+      content,
+    },
+  }
 }
