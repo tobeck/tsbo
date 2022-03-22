9067d6068547064a35c1ea9be83161c0c29eaede
tobeck <tobias.becker.olsson@gmail.com>
2022-03-22 14:17:11 +0100

    Blog in text-black

--- components/intro.js
+++ components/intro.js
@@ -3,7 +3,7 @@ import { CMS_NAME } from '../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-black text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
@@ -14,7 +14,7 @@ export default function Intro() {
         >
           Next.js
         </a>{' '}
-        and {CMS_NAME}.
+        and {CMS_NAME}. This part of the page is in the intro.js
       </h4>
     </section>
   )
