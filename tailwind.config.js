1286abf32eb7fd57bb2687b2e5734de5f0edfa67
tobeck <tobias.becker.olsson@gmail.com>
2022-04-06 16:03:46 +0200

    Set mx-auto in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         ash: '#A8B7AB',
         bistre: '#251605',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: ['@tailwindcss/typography'],
 }
