a2e293b7b056e25d863c3270f6c7475c18afc5ed
tobeck <tobias.becker.olsson@gmail.com>
2022-04-29 14:46:19 +0200

    Added tsbo_logo, upd navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row justify-end gap-5 text-xs font-bold uppercase text-bistre">
+    <nav className="container flex items-center py-5 mt-10">
       <div>
-        <h1></h1>
+        <Link href="/">
+          <Image src="/assets/TSBO_logo.svg" width="64" height="64" />
+        </Link>
       </div>
-      <ul>
+      <ul className="hidden sm:flex flex-1 justify-end items-center gap-5  text-xs">
         <Link href="/experiences">
           <a>Experiences</a>
         </Link>
@@ -14,6 +19,9 @@ export default function Navbar() {
           <a>Blog</a>
         </Link>
       </ul>
+      <div className="flex sm:hidden flex-1 justify-end">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </nav>
   )
 }
